ad881dd7d4768101556ae305949aa8dc2d03f8d3
ayushatindra <116113390+ayushatindra@users.noreply.github.com>
2024-04-19 00:47:39 +0530

    Add files via upload

---
+++ JS level2/script.js
@@ -0,0 +1,128 @@
+// single line commment
+// 
+/*
+multi line  comments
+
+*/
+// arithmentic
+// let a =5
+// let b=10;
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+// // post /pre operator
+
+// console.log(a++)
+// console.log(a--)
+// console.log(++a)
+// console.log(a++)
+// // Assignment operator
+// a=6;
+// b=7;
+// a+=5;
+// coparision operator
+
+
+// a=6;
+// b="6";
+// console.log(a===b);-->false
+// a=6;
+// b="6";
+// console.log(a==b);->true
+
+
+// logical operator
+// let a =7;
+// let b=6;
+
+// let cond1=a>b; // true
+// let cond2=a==b; //false
+
+// console.log(cond1|| cond2);  -->pipe symbol
+// console.log(cond1&& cond2);
+
+// conditioal statemenr
+// let age=12;
+// if(age>=18){
+//   console.log("vote ")
+// }
+
+// if(age<=18){
+//   console.log("not vote ")
+// }
+
+// let mode="dark";
+// if(mode=="dark"){
+//   color="black"
+// }
+// else{
+//   color="white"
+// }
+// whether number is odd / even
+// let num=4;
+
+// if(num%2==0){
+//   console.log("even")
+// }
+// else{
+//   console.log("odd")
+// }
+
+// // syntax ->rules
+// let mode="dark";
+// let color;
+// if(mode=="dark"){
+//   color="black"
+// }
+// else if(mode=="blue"){
+//   color="blue";
+// }
+// else if(mode="pink"){
+//   color="pink"
+// }
+// else{
+//   color ="white"
+// }
+
+// ternary operator
+// let age=25;
+// let result=age>=25?"adult":bachaa
+// console.log(result)
+// practice problem
+
+// let name=prompt("enter your name") 
+// console.log("hello ",name)
+
+// let num =prompt("Enter a number ");
+// if(num% 5===0){
+//   console.log("You get your luckey number")
+// }
+// else{
+//   console.log("better luck next time")
+// }
+
+// q2 grading problem
+let grade=969
+
+if(grade<=100 && grade>=80){
+  console.log("GRADE- A")
+}
+else if(grade<=89 && grade>=70){
+  console.log("GRADE- B")
+}
+else if(grade<=69 && grade>=60){
+  console.log("GRADE- C")
+}
+else if(grade<=59 && grade>=50){
+  console.log("GRADE- D")
+}
+else{
+  console.log("GRADE-F")
+}
+
+
+
+
