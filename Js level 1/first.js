de537865c8e337298cac99451135e2b61dfd7de5
ayushatindra <116113390+ayushatindra@users.noreply.github.com>
2024-04-18 19:41:56 +0530

    Add files via upload

---
+++ Js level 1/first.js
@@ -0,0 +1,72 @@
+// console.log("ayush");
+// console.log("Let's start javaScript");
+// fullName="Ayush";
+// console.log(fullName)
+// price =99.9;
+// console.log(price);
+// radious=3;
+// console.log(radious);
+// a=null;
+// b=undefined;
+// console.log(a);
+// console.log(b)
+
+// isFollow=false;
+// console.log(isFinite)
+
+// full_name="ayush";
+// fullName="ayush";
+// FullName123="ayush";
+
+
+// let nameFull="ayush";
+// let name="ayush";
+// let age1=23;
+
+
+// var age=78;
+// var age=90;
+// var age=90;
+// console.log(age);
+
+// {
+//   a=5;
+//   console.log(a)
+// }
+// {
+//   a=10;
+//   console.log(a)
+// }
+
+
+// let price=24;
+// // let age=21;
+// // console.log("ayush")
+
+// const student={
+//   age1: 20,
+//   name1: "ayush",
+//   isPass: true,
+// };
+
+// console.log(student['name'])
+
+
+// problem 1
+const product={
+  name:"Parker",
+  rating:4,
+  deal:"no",
+  offer:5,
+  price:270,
+
+};
+
+
+const InstaProfile={
+  name1:"Ayush",
+  follower:234,
+  isfollow:true,
+  following:232,
+  bio:"Hi this is my insta account"
+};
